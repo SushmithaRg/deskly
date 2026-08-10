f77c635e6cc705739d724c62623cd4af0a46c3cf
sushmithargsush-cpu <sushmithargsush@gmail.com>
2026-08-10 22:53:58 +0530

    Simplify Vercel API handler

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,5 @@
-import type { Request, Response } from "express";
-
-export default function handler(_req: Request, res: Response) {
-  return res.status(200).json({
+export default function handler(req: any, res: any) {
+  res.status(200).json({
     success: true,
     message: "Vercel API function is working"
   });
