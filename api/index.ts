65cbd5e41af173f3783576a6b73ff09a598d4052
sushmithargsush-cpu <sushmithargsush@gmail.com>
2026-08-10 22:43:16 +0530

    Test Vercel API function

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,8 @@
-import app from "../src/backend/server";
+import type { Request, Response } from "express";
 
-export default app;
+export default function handler(_req: Request, res: Response) {
+  return res.status(200).json({
+    success: true,
+    message: "Vercel API function is working"
+  });
+}
