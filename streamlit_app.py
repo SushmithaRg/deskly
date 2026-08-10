02e4aa28f644118c724c9a8c5d562d68a1da5639
Sushmitha R G <sushmitharg26@gmail.com>
2026-08-10 10:27:33 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Deskly",
+    page_icon="💼",
+    layout="wide"
+)
+
+st.title("💼 Deskly")
+st.subheader("Company Management System")
+
+st.write("Welcome to Deskly!")
